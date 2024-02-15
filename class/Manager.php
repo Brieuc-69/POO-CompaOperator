06b6e2aa99584189750bffee576b0a32c9e43206
Brieuc-69 <jamesoncherry243@gmail.com>
2024-02-15 09:27:50 +0100

    branche brieuc

--- class/Manager.php
+++ class/Manager.php
@@ -30,28 +30,77 @@ class Manager {
   
 
 
-    public function creatReview() {
-        $request = $this->bdd->prepare('INSERT INTO ')
-
+    function createReview($data){
+        $request = $this->bdd->prepare('INSERT INTO review(location, price, tourOperatorId) VALUES(:location, :price, :tourOperatorId)');
+        $request->execute(array(
+            'location' => $data['location'],
+            'price' => $data['price'],
+            'tourOperatorId' => $data['tourOperatorId']
+        ));
     }
 
-    public function getReviewByOperatorByOperatorid(SELECT * FROM ) {
+    public function getReviewByOperatorByOperatorid($id) {
 
+        $request = $this->bdd->prepare('SELECT * FROM review WHERE tour_operator_id = :id');
+        $request->execute([
+            'id' => $id
+        ]);
+
+        $reviews = $request->fetchAll();
+
+        return $reviews;
+    
     }
+    
 
-    public function getAllOperator() {
+    function getAllOperator(){
 
+        $request = $this->bdd->query('SELECT * FROM tour_operator');
+        $tourOperators = $request->fetchAll();
+        return $tourOperators;
+    
+    
     }
     
     public function updateOperatorToPremium() {
 
+        function updateOperatorToPremium(){
+
+            $req = $this->bdd->prepare('UPDATE tour_operator SET isPremium = 1 WHERE id = :id');
+            $req->execute(array(
+                'id' => $id
+            ));
+        
+        }
+
     }
 
-    public function createTourOperator() {
+    function createTourOperator(){
 
+        $request = $this->bdd->prepare('INSERT INTO tour_operator(name, grade, link, isPremium, destinationId) VALUES(:name, :grade, :link, :isPremium, :destinationId)');
+        $request->execute(array(
+            'name' => $data['name'],
+            'grade' => $data['grade'],
+            'link' => $data['link'],
+            'isPremium' => $data['isPremium'],
+            'destinationId' => $data['destinationId']
+        ));
+    
+    
     }
 
     public function crateDestination() {
+
+         function createDestination(){
+
+
+            $req = $this->bdd->prepare('INSERT INTO destination(name) VALUES(:name)');
+            $req->execute(array(
+                'name' => $data['name']
+            ));
+        
+        
+        }
 
     }
         
