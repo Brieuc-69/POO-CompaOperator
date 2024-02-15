7f0ea7c2ddf72c38493afcea3f2b204781a59522
alexis-moreno-lopez <alexis.morenolopezpro@gmail.com>
2024-02-15 17:26:41 +0100

    rome

--- index.php
+++ index.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css" />
+    <title>Voyage</title>
 </head>
 <body>
-    
+<header></header>
+<main>
+
+<section id="connexion"> 
+
+<div class="box">
+<form action="./accueil.php" method="">
+
+    <input placeholder="Pseudo" type="text">
+    <div class="btnconnexion">
+    <button type="submit" name="validation">Go</button>
+</div>
+</form>
+</div>
+
+</section>
+
+</main>
+<footer></footer>
 </body>
 </html>
---
+++ rome.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+    <header id="header">
+    <div>
+
+      <img class="imglogo" src="./img/travel.jpg" alt="" />
+    </div>
+
+    <nav>
+      <ul>
+        <li>Accueil</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+
+    </header>
+
+
+
+    <main>
+        <section id="fond">
+            <div class="assemble">
+            <div class="booking">
+            <button class="btntarif"></button>
+            </div>
+            <div class="booking1">
+            <button class="btntarif"></button>
+            </div>
+            <div class="booking2">
+               <button class="btntarif">10 000$</button>
+            </div>
+        </div>
+        </section>
+    </main>
+
+
+
+    <footer></footer>
+
+</body>
+
+</html>
